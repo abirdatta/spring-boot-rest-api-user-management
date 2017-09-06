c0e0d2c6967eae5bd05150498cac7bd0b51b15ae
Abir Datta <abirdatta.in@gmail.com>
2017-09-06 21:26:18 +0530

    added docker kubernetes support

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM java
+MAINTAINER abirdatta
+COPY ./target/user-service-0.0.1-SNAPSHOT.jar /usr/local/user-api/
+RUN ls -al /usr/local/user-api
+EXPOSE 8080
+VOLUME /logs
+WORKDIR /usr/local/user-api
+ENTRYPOINT java -jar user-service-0.0.1-SNAPSHOT.jar
---
+++ init-mysql.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE mydb;
+
+GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'%' IDENTIFIED BY 'mysql';
+
+GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'localhost' IDENTIFIED BY 'mysql';
+
+USE mydb;
+
+CREATE TABLE user_type (user_type_id INT NOT NULL AUTO_INCREMENT, type VARCHAR(100), description VARCHAR(100), PRIMARY KEY (user_type_id));
+
+CREATE TABLE user (user_id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(100), middle_name VARCHAR(100), last_name VARCHAR(100), email_id VARCHAR(100), password VARCHAR(100), city VARCHAR(100), country VARCHAR(100), user_type_id INT, PRIMARY KEY (user_id), CONSTRAINT FK_user_type FOREIGN KEY (user_type_id) REFERENCES user_type(user_type_id));
+
+INSERT INTO user_type (type, description) VALUES ("broker", "This is a broker type of user");
+
+INSERT INTO user_type (type, description) VALUES ("owner", "This is a owner type of user");
